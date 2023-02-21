5ff3fe6308b5d9af6f2ff1b6f03f3c7910b9aaba
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-02-21 13:19:00 +0600

    first commit

---
+++ back/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'token' => Hash::make($request->password)
+        ];
+        $user = User::create($data);
+        return response($user)->header("Access-Control-Allow-Origin", config('cors.allowed_origins'))
+            ->header("Access-Control-Allow-Methods", config('cors.allowed_methods'));
+    }
+}
